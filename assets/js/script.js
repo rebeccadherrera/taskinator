1433f219667e48b2d7485de6c0ce2c67f5ee5baf
Rebecca Herrera <rebeccadherrera@gmail.com>
2021-11-14 15:16:28 -0500

    added form values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,26 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var creatTaskHandler = function (event) {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.dir(taskTypeInput);
+
+    // create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info"
+    //add html content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='taek-type'>" + taskTypeInput + "</span>";
+
+    listItemE1.appendChild(taskInfoE1);
+
+    //add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
