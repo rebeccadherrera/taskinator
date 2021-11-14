022ee40a898192a799214f027d8c43a74d8171ab
Rebecca Herrera <rebeccadherrera@gmail.com>
2021-11-14 14:52:51 -0500

    updated from event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var creatTaskHandler = function () {
+var creatTaskHandler = function (event) {
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
-}
+};
 
-buttonE1.addEventListener("click", creatTaskHandler);
+formE1.addEventListener("submit", creatTaskHandler);
